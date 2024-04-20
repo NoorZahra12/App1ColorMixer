691236d00f135b9c939efd6cce404465f6be42a9
Noor Zahra <114682660+NoorZahra12@users.noreply.github.com>
2024-04-20 04:33:31 +0400

    adding comments

--- app/src/main/java/com/example/colormixer/MainActivity.kt
+++ app/src/main/java/com/example/colormixer/MainActivity.kt
@@ -16,6 +16,7 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+//    storing all id of the colors in a list by id
     private val colorIds = listOf(
         R.id.userColorBoxR,
         R.id.userColorBoxY,
@@ -26,7 +27,9 @@ class MainActivity : AppCompatActivity() {
         R.id.userColorBoxBlack,
         R.id.userColorBoxWhite
     )
+//    giving values to all colors. they will represent the unit of color when mixing but set to 0 by default
     private val colorValues = MutableList(colorIds.size) { 0 }
+//    reducing the index by -1 so it's starting from 1,2,3 instead of 0 even though i can do it with 0 too but i just wanted it to start with 1 for my own reason so it's easier to remember
     private var selectedColorIndex: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,13 +37,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-//        finding the ui elements by calling their ids
+//        finding the ui element for instruction button by calling their ids
         val howButton = findViewById<Button>(R.id.howButton)
 //        Setting up click listener to show instruction pop up if user clicks
         howButton.setOnClickListener {
             showInstructionPopUp()
         }
-
+//        finding the ui elements for the rest by calling their ids
         val plusButton = findViewById<Button>(R.id.plusButton)
         val minusButton = findViewById<Button>(R.id.minusButton)
         val resetButton = findViewById<Button>(R.id.resetButton)
@@ -48,30 +51,32 @@ class MainActivity : AppCompatActivity() {
         val progressBar = findViewById<ProgressBar>(R.id.colorMatchBar)
         val satisfactionImageView = findViewById(R.id.satifactionlvlemoji) as ImageView
 
-        // Set up click listeners for each color view
         for (i in colorIds.indices) {
+//        setting up click listeners for each color
             findViewById<View>(colorIds[i]).setOnClickListener {
+//                take input of the selected color
                 selectedColorIndex = i
+//                changing the ui like match percentage, emoji, progressbar and colorbox of user to match target
                 updateUI()
             }
         }
 
-        // Plus button click listener
+//         Plus button click listener this will update user color box
         plusButton.setOnClickListener {
             if (selectedColorIndex != -1) {
                 colorValues[selectedColorIndex]++
-                // Update the progress bar
+//                 Update the progress bar
                 updateProgressBar()
                 updateUI()
                 satisfactionface()
             }
         }
 
-        // Minus button click listener
+//         Minus button click listener this will update user color box
         minusButton.setOnClickListener {
             if (selectedColorIndex != -1 && colorValues[selectedColorIndex] > 0) {
                 colorValues[selectedColorIndex]--
-                // Update the progress bar
+//                 Update the progress bar
                 updateProgressBar()
                 updateUI()
                 satisfactionface()
@@ -79,7 +84,7 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        // Reset button click listener
+//         Reset button click listener
         resetButton.setOnClickListener {
             colorValues.fill(0)
             selectedColorIndex = -1
@@ -90,9 +95,11 @@ class MainActivity : AppCompatActivity() {
             satisfactionImageView.setImageResource(R.drawable.happy)
         }
 
+//        Subbmit button will simply give a pop of showing result and option to change color or stay to try and get better result
         submitButton.setOnClickListener {
             val currentPercentage = progressBar.progress
             val message = when {
+//                a message in the pop up for different percentage
                 currentPercentage < 30 -> "You're far from the target color. Keep trying!"
                 currentPercentage < 60 -> "You're making progress, but there's still room for improvement."
                 currentPercentage < 80 -> "You're getting closer to the target color. Keep it up!"
@@ -138,10 +145,10 @@ class MainActivity : AppCompatActivity() {
             3. Remember to have fun!
             
             """.trimIndent()
-
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Instructions")
             .setMessage(instructions)
+//            pressing ok will close popup
             .setPositiveButton("OK") { dialog, _ ->
                 dialog.dismiss()
             }
@@ -153,19 +160,21 @@ class MainActivity : AppCompatActivity() {
         val userColorBox = findViewById<View>(R.id.userColorBox)
 //        val selectedColorBox = findViewById<View>(colorIds[selectedColorIndex])
 
-        // Update the color unit count text
+//        by checking if selectedColorIndex is not equal to -1, the code ensures that it only attempts to access the colorValues array when a color is actually selected.
         if (selectedColorIndex != -1) {
+//            updating unit count of a color after clicking it
             counterintuitive.text = colorValues[selectedColorIndex].toString()
         } else {
+//            otherwise the the unit counter is not updated
             counterintuitive.text = ""
         }
 
-        // Update the background color of the user color box
+//         Updating the background color of the user color box
         val color = calculateMixedColor()
         userColorBox.setBackgroundColor(color)
     }
 
-    // Function to calculate the mixed color based on color values
+//     Function to calculate the mixed color based on color values
     private fun calculateMixedColor(): Int {
         var totalRed = 0
         var totalGreen = 0
@@ -174,6 +183,7 @@ class MainActivity : AppCompatActivity() {
         for (i in colorIds.indices) {
             val color = getColorForIndex(i)
             val value = colorValues[i]
+//            now all the rbg values in the color will be calculated according to how much user changes the units of color and return the final result
             totalRed += Color.red(color) * value
             totalGreen += Color.green(color) * value
             totalBlue += Color.blue(color) * value
@@ -183,7 +193,8 @@ class MainActivity : AppCompatActivity() {
         return if (totalUnits > 0) {
             Color.rgb(totalRed / totalUnits, totalGreen / totalUnits, totalBlue / totalUnits)
         } else {
-            Color.WHITE // Default color if no units are selected
+//            default white color
+            Color.WHITE
         }
     }
 
@@ -197,13 +208,15 @@ class MainActivity : AppCompatActivity() {
         val matchPercentageNum = findViewById<TextView>(R.id.userColorTextView)
 
         if (userColor != null && targetColor != null) {
+//            getting percentage and storing in variable
             val percentageMatch = getMatchPercentage(userColor, targetColor)
+//            displaying the result of percentage with 1 digit after the decimal point in string form
             progressBar.progress = percentageMatch.toInt()
             matchPercentageNum.text = String.format("Your Color: %.1f%%", percentageMatch)
         }
     }
 
-    // Function to get the color for the selected color index
+//     Function to get the color for the selected color index
     private fun getColorForIndex(index: Int): Int {
         return when (index) {
             0 -> Color.RED
@@ -218,7 +231,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // Function to calculate color match percentage
+//     Function to calculate color match percentage
     private fun getMatchPercentage(userColor: Int, targetColor: Int): Double {
         val userRed = Color.red(userColor)
         val userGreen = Color.green(userColor)
@@ -226,21 +239,26 @@ class MainActivity : AppCompatActivity() {
         val targetRed = Color.red(targetColor)
         val targetGreen = Color.green(targetColor)
         val targetBlue = Color.blue(targetColor)
-
-        val totalDiff = (Math.abs(userRed - targetRed) +
-                Math.abs(userGreen - targetGreen) +
-                Math.abs(userBlue - targetBlue)).toDouble()
+//    formula to get the difference from the target color and seeing how close or far user is from matching the target
+        val totalDiff = (Math.abs(userRed - targetRed) + Math.abs(userGreen - targetGreen) + Math.abs(userBlue - targetBlue)).toDouble()
         val maxDiff = 3 * 255.0
+//    returning the difference in percentage
         return ((maxDiff - totalDiff) / maxDiff) * 100
     }
     fun satisfactionface() {
         val satisfactionImageView = findViewById(R.id.satifactionlvlemoji) as ImageView
         val progressBar = findViewById<ProgressBar>(R.id.colorMatchBar)
         when {
+//            based on the progress of progress bar different emoji will appear
+//            emoji will be sad if the percentage is below 20%
             progressBar.progress < 20 -> satisfactionImageView.setImageResource(R.drawable.cry)
+//            emoji is happy between 20-40%
             progressBar.progress < 40 -> satisfactionImageView.setImageResource(R.drawable.sad)
+//            emoji is happy between 40-60%
             progressBar.progress < 60 -> satisfactionImageView.setImageResource(R.drawable.neutralemoji)
+//            emoji is happy between 60-80%
             progressBar.progress < 80 -> satisfactionImageView.setImageResource(R.drawable.happy)
+//            emoji is excited if above 80%
             else -> satisfactionImageView.setImageResource(R.drawable.excited)
         }
     }
